72965b3f0b5a309e05c3c8208f1b0a690ce5f6c2
Areum <areumjo1@gmail.com>
2019-07-26 12:52:27 -0400

    Fetched character API and displayed it to the list

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [data, setData] = useState([]);
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    Axios
+      .get(`https://rickandmortyapi.com/api/character`)
+      .then(response => {
+        console.log(response.data.results)
+        const data = response.data.results;
+        setData(data)
+      })
   }, [])
-
   return <section className='character-list grid-view'>
 
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {/* <h2>TODO: `array.map()` over your state here!</h2> */}
+      {data.map(d => {
+        return (
+          <div className="character-list" key={d.id}>
+            <img src={d.image} alt="Rick-Morty fan page" />
+            <h3>{d.name}</h3>
+            <h3>{d.status}</h3>
+          </div>
+        )
+      })}
     </section>
 
 }
